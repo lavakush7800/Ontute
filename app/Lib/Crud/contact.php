7b44a1d9c651a1e7e7ab21639e08c6b1e9e3ac72
lavakush7800 <lavakushgiri@gmail.com>
2021-05-07 16:18:40 +0530

    5

---
+++ app/Lib/Crud/contact.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Lib\Crud;
+use App\Http\Controllers\Crud\ContactController;
+use App\Models\Contact as Model;
+use Illuminate\Support\Facades\Log;
+
+class Contact{
+    public static function store(){
+        try{
+            
+        }catch(\Exception $e){
+
+        }
+    }
+}
