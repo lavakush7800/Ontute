3dea548c3a73507d9743921e2d07ec229b71c93a
lavakush7800 <lavakushgiri@gmail.com>
2021-06-24 02:45:16 +0530

    super

---
+++ resources/views/superadmin.blade.php
@@ -0,0 +1,1 @@
+<h1>super admin</h1>
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user', function () {
     return view('user');
 });
+
+Route::view('superadmin','superadmin');
