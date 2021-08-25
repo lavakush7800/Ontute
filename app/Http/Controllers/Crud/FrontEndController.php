20e1e9702ec09147c6a306d82d103d4e61c85ac0
lavakush7800 <lavakushgiri@gmail.com>
2021-08-25 17:23:04 +0530

    store

--- app/Http/Controllers/Crud/FrontEndController.php
+++ app/Http/Controllers/Crud/FrontEndController.php
@@ -10,4 +10,11 @@ class FrontEndController extends Controller
     public function index(){
         return view('front');
     }
+    public function store(Request $request){
+        try{
+
+        }catch(Exeption $e){
+
+        }
+    }
 }
